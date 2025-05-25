6a1853172a15b3a7843137c8a74210980ab8ba87
laras <larasatilituhayumrjn@gmail.com>
2025-05-25 15:37:19 +0700

    benerin error

--- routes/realtime.js
+++ routes/realtime.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getRealtimeStats } = require('../controllers/realtimeStatsController');
+const { getRealtimeStats } = require('../controllers/realtimeController');
 const authenticate = require('../middleware/authMiddleware');
 
 router.get('/', authenticate, getRealtimeStats);
