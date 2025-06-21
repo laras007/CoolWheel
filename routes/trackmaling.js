f52b726bf1e1f8c3dff0afee07f96cbd6c2465a1
laras <larasatilituhayumrjn@gmail.com>
2025-06-21 12:24:56 +0700

    parking

--- routes/trackmaling.js
+++ routes/trackmaling.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const trackingController = require('../controllers/trackimalingController');
+const trackingController = require('../controllers/trackmalingController');
 
 router.post('/', trackingController.trackLocation); // POST /api/tracking
 
