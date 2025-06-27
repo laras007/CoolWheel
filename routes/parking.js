0534d994323d1dfe80c2f4d4e2eb2d035191cfa9
laras <larasatilituhayumrjn@gmail.com>
2025-06-28 02:40:18 +0700

    midelwer

--- routes/parking.js
+++ routes/parking.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const parkingController = require('../controllers/parkingController');
+const authenticateToken = require('../middleware/authMiddleware');
 
 // POST /api/parking/toggle
 router.post('/toggle', parkingController.toggleParking);
